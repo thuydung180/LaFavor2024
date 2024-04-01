a118f1e0b4e5fc39a607673f7f8dffe17623c4c4
thuydung180 <dungttt21411@st.uel.edu.vn>
2024-04-02 03:25:36 +0700

    add address done

---
+++ app/src/main/java/com/nhom5/lafavor2024/MyAddressAdd.java
@@ -0,0 +1,14 @@
+package com.nhom5.lafavor2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAddressAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_address_add);
+    }
+}
