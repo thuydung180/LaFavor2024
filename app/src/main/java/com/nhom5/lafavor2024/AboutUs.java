c013b8927a79944a5773a8c263d07f9905e374c5
thuydung180 <dungttt21411@st.uel.edu.vn>
2024-04-02 02:10:38 +0700

    my address item done

--- app/src/main/java/com/nhom5/lafavor2024/AboutUs.java
+++ app/src/main/java/com/nhom5/lafavor2024/AboutUs.java
@@ -3,12 +3,21 @@ package com.nhom5.lafavor2024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.nhom5.lafavor2024.databinding.ActivityAboutUsBinding;
 
 public class AboutUs extends AppCompatActivity {
+
+    ActivityAboutUsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        binding = ActivityAboutUsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imvAboutUs.setClipToOutline(true);
+        binding.imvAboutUs.setScaleType(ImageView.ScaleType.CENTER_CROP);
     }
 }
