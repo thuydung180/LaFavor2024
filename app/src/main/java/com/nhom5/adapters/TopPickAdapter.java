e4b6197a42e500c0c017e09f4e9095bb890d2a59
thuydung180 <dungttt21411@st.uel.edu.vn>
2024-04-18 02:02:32 +0700

    product detail update

--- app/src/main/java/com/nhom5/adapters/TopPickAdapter.java
+++ app/src/main/java/com/nhom5/adapters/TopPickAdapter.java
@@ -1,6 +1,8 @@
 package com.nhom5.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.nhom5.lafavor2024.HomeFragment;
 import com.nhom5.lafavor2024.MainActivity;
+import com.nhom5.lafavor2024.ProductDetail;
 import com.nhom5.lafavor2024.R;
 import com.nhom5.lafavor2024.databinding.FragmentHomeBinding;
 import com.nhom5.models.Product;
@@ -49,8 +52,17 @@ public class TopPickAdapter extends RecyclerView.Adapter<TopPickAdapter.ViewHold
         Picasso.get().load(product.getProductUrl()).into(holder.imvProduct);
         holder.txtName.setText(product.getProductName());
         holder.txtPrice.setText(String.format("%d VNĐ", (int) product.getProductPrice()));
+        holder.txtComparePrice.setText(String.format("%d VNĐ", (int) product.getProductComparePrice()));
+        holder.txtComparePrice.setPaintFlags(holder.txtComparePrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         holder.txtRating.setText(String.valueOf(product.getProductRating()));
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(fragment.getContext(), ProductDetail.class);
+                fragment.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -104,7 +116,7 @@ public class TopPickAdapter extends RecyclerView.Adapter<TopPickAdapter.ViewHold
 //
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imvProduct;
-        TextView txtName, txtPrice, txtRating;
+        TextView txtName, txtPrice, txtRating, txtComparePrice;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -112,6 +124,7 @@ public class TopPickAdapter extends RecyclerView.Adapter<TopPickAdapter.ViewHold
             txtName = itemView.findViewById(R.id.txtProductName);
             txtPrice = itemView.findViewById(R.id.txtPrice);
             txtRating = itemView.findViewById(R.id.txtRating);
+            txtComparePrice = itemView.findViewById(R.id.txtComparePrice);
         }
     }
 }
--- app/src/main/java/com/nhom5/lafavor2024/ProductDetail.java
+++ app/src/main/java/com/nhom5/lafavor2024/ProductDetail.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nhom5.lafavor2024.databinding.ActivityProductDetailBinding;
+
 public class ProductDetail extends AppCompatActivity {
+
+    ActivityProductDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_detail);
+        binding = ActivityProductDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
